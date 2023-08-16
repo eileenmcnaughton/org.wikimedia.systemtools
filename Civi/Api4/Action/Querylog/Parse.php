2a54b5a5538396f84317dc80abf8b210fb008f62
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-08-16 17:47:26 +1200

    Various updates (opps it's been a while)

--- Civi/Api4/Action/Querylog/Parse.php
+++ Civi/Api4/Action/Querylog/Parse.php
@@ -47,7 +47,7 @@ class Parse extends AbstractAction {
     $lines = file($this->getFileName(), FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     $parsed = [];
     $writer = Writer::createFromPath(dirname($this->getFileName()) . '/query_log_parsed.csv', 'w+');
-    $writer->insertOne(['Date', 'Query', 'Seconds taken', 'Affected rows', 'Affected columns']);
+    $writer->insertOne(['Date', 'Query', 'Seconds taken', 'Affected rows', 'Affected columns', 'Deleted Records', 'Updated Records', 'Inserted Records']);
 
     $currentIndex = 0;
     foreach ($lines as $index => $line) {
@@ -55,7 +55,7 @@ class Parse extends AbstractAction {
       $date = substr($line,0,15);
       if (preg_match("/^[A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$/",$date)) {
         // We have a new log line.
-        $date = date('Y-m-d H:i:s');
+        $date = date('Y-m-d H:i:s', strtotime($date));
         $type = substr($line, 18, 5) === 'debug' ? 'debug' : 'info';
         $isQuery = substr($line, 24, 9) === ' $Query =';
         if ($isQuery) {
@@ -64,22 +64,37 @@ class Parse extends AbstractAction {
             $writer->insertOne($parsed[$currentIndex]);
           }
          // this is the low hanging fruit of data sanitising - get rid of emails.
-          $query = preg_replace('/([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/', $currentIndex . '@example.com', $query);
+          $query = preg_replace('/([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/', $currentIndex . '@example.com', substr($line, 34));
           $parsed[$index] = [
             'timestamp' => $date,
             'query' => $query,
             'seconds' => '',
             'rows' => '',
             'columns' => '',
+            'deleted' => '',
+            'updated' => '',
+            'inserted' => '',
           ];
           $currentIndex = $index;
         }
         elseif ($type === 'info') {
-          $re = '/QUERY DONE IN (\d*.\d*)  seconds. Result is (\d*) rows by (\d*) columns./m';
+          $re = '/QUERY DONE IN (\d*.\d*)  seconds./m';
           preg_match_all($re, $line, $matches, PREG_SET_ORDER, 0);
           $parsed[$currentIndex]['seconds'] = $matches[0][1];
-          $parsed[$currentIndex]['rows'] = $matches[0][2];
-          $parsed[$currentIndex]['columns'] = $matches[0][2];
+          $re = '/Result is (\d*) rows by (\d*) columns./m';
+          preg_match_all($re, $line, $matches, PREG_SET_ORDER, 0);
+          $parsed[$currentIndex]['rows'] = $matches[0][1];
+          $parsed[$currentIndex]['columns'] = $matches[0][2] ?? '';
+          $re = '/(\d*) row\(s\)s subject to delete action/m';
+          preg_match_all($re, $line, $matches, PREG_SET_ORDER, 0);
+          $parsed[$currentIndex]['deleted'] = $matches[0][1] ?? '';
+          $re = '/(\d*) row\(s\)s subject to update action/m';
+          preg_match_all($re, $line, $matches, PREG_SET_ORDER, 0);
+          $parsed[$currentIndex]['updated'] = $matches[0][1] ?? '';
+
+          $re = '/(\d*) row\(s\)s subject to insert action/m';
+          preg_match_all($re, $line, $matches, PREG_SET_ORDER, 0);
+          $parsed[$currentIndex]['inserted'] = $matches[0][1] ?? '';
         }
 
       }
